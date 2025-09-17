6e5c4ebabca02c8d1f864b78dd6412433bd97b89
Rodolphe Levesque <rodolphe.levesque@gmail.com>
2025-09-17 23:43:23 +0200

    feat: Force database recreation

--- backend/sql_app/database.py
+++ backend/sql_app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # L'URL de notre base de données. "matrix.db" sera un fichier créé dans le même dossier.
-SQLALCHEMY_DATABASE_URL = "sqlite:///./matrix.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./matrix_v2.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
